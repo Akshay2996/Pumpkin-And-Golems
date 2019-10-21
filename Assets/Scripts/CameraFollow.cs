01114d078a348ce7d894c00feac8b97e8d470bf1
mohitjn <jn.mht@gmail.com>
2019-10-21 23:41:43 +0530

    Unity Game

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public Transform pumpkinPos;
+    private float yDistance = 4f;
+    private float zDistance = 7f;
+
+    void Awake ()
+    {
+       pumpkinPos = GameObject.Find("Pumpkin").transform;
+	}
+
+    // Update is called once per frame
+    void Update ()
+    {
+        Vector3 temp = transform.position;
+        temp.y = pumpkinPos.position.y + yDistance;
+        temp.z = pumpkinPos.position.z - zDistance;
+        transform.position = temp;
+	}
+}
---
+++ Assets/Scripts/Pumpkin_Move.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pumpkin_Move : MonoBehaviour {
+
+    public float moveForce = 10f;
+    private Rigidbody myBody;
+    //Awake Runs Before Start function
+    void Awake()
+    {
+        myBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+		if(transform.position.y < -5)
+        {
+            Time.timeScale = 0f;
+        }
+	}
+    //FixUpdate is called once 1/3 or 1/4 per frame
+    void FixedUpdate()
+    {
+        MovePumpkin();
+    }
+
+    void MovePumpkin()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        myBody.AddForce(new Vector3(h * moveForce, 0f ,v * moveForce));
+    }
+    void OnTriggerEnter(Collider target)
+    {
+        if (target.tag == "Golem" || target.tag == "Gates")
+        {
+            Time.timeScale = 0f;
+        }
+    }
+}
